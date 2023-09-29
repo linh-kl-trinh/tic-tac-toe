f3fe871b534011a314ca04203a4ea9ceb2f126f1
Linh Trinh <tklinh202@gmail.com>
2023-09-29 04:28:00 -0700

    Add logic to make move

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-# Tic Tac Toe with AI Opponent
+# Tic Tac Toe with Opponent
 
 ## Description
-This is a simple two-player (player vs. AI opponent) Tic Tac Toe game written in Python. The game board is a 3x3 grid and players take turns marking a space with their respective symbol ("X" or "O"). The game ends when a player gets three of their symbols in a row (horizontally, vertically, or diagonally), or if all spaces on the board are filled without either player winning.
-
-## Functions
-
-- `print_board(board)`: This function takes in the current state of the game board as a list of lists and prints it out in a visually pleasing format. It does this by iterating over each row and column of the board and printing out the corresponding symbol or empty space.
-- `check_win(board, player)`: This function takes in the current state of the game board and the symbol of the current player and checks if that player has won the game. It does this by checking all possible winning configurations (horizontally, vertically, and diagonally) and returning `True` if any of them match the current player's symbol.
-- `get_ai_move(board)`: This function takes in the current state of the game board and returns the row and column of the AI's next move. To do this, it generates random row and column numbers until an empty cell is found.
-- `tic_tac_toe()`: This function initializes the game board, sets up the players and their symbols, and starts the game loop. It alternates between each player's turn and prompts them to input a row and column to mark their symbol on the board. It then checks if the game has been won or if it is a tie, and prints out the corresponding message before ending the game.
+This is a simple two-player (player vs. opponent) Tic Tac Toe game written in Python. The game board is a 3x3 grid and players take turns marking a space with their respective symbol ("X" or "O"). The game ends when a player gets three of their symbols in a row (horizontally, vertically, or diagonally), or if all spaces on the board are filled without either player winning.
 
 ## How to play
 To play the game, simply run the `main.py` file in a Python environment or from the command line.
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import random
 
-def get_ai_move(board):
-    while True:
-        row = random.randint(0, 2)
-        col = random.randint(0, 2)
-        if board[row][col] == " ":
+def get_empty_cells(board):
+    return [(i, j) for i in range(3) for j in range(3) if board[i][j] == " "]
+
+def get_opponent_move(board):
+    empty_cells = get_empty_cells(board)
+    
+    for cell in empty_cells:
+        row, col = cell
+        board[row][col] = "O"
+        if check_win(board, "O"):
             return row, col
+        board[row][col] = " "
+
+    for cell in empty_cells:
+        row, col = cell
+        board[row][col] = "X"
+        if check_win(board, "X"):
+            return row, col
+        board[row][col] = " "
+
+    return random.choice(empty_cells)
 
 def print_board(board):
     print("-------------")
@@ -29,29 +44,41 @@ def check_win(board, player):
     return False
 
 def tic_tac_toe():
-    board = [[" ", " ", " "] for i in range(3)]
+    board = [[" " for _ in range(3)] for _ in range(3)]
     players = ["X", "O"]
-    current_player = players[0]
+    current_player = random.choice(players)
+    
+    print("Welcome to Tic-Tac-Toe!")
     print_board(board)
+    
     while True:
-        print("It's", current_player, "'s turn.")
+        print(f"It's {current_player}'s turn.")
+        
         if current_player == "X":
-            row = int(input("Enter row (1, 2 or 3): ")) - 1
-            col = int(input("Enter column (1, 2 or 3): ")) - 1
+            while True:
+                try:
+                    row = int(input("Enter row (1, 2, or 3): ")) - 1
+                    col = int(input("Enter column (1, 2, or 3): ")) - 1
+                    if (0 <= row < 3) and (0 <= col < 3) and board[row][col] == " ":
+                        break
+                    else:
+                        print("Invalid move, try again.")
+                except ValueError:
+                    print("Invalid input, please enter a number.")
         else:
-            row, col = get_ai_move(board)
-            print("AI chooses row", row+1, "and column", col+1)
-        if board[row][col] != " ":
-            print("Invalid move, try again.")
-            continue
+            row, col = get_opponent_move(board)
+            print(f"Opponent chooses row {row + 1} and column {col + 1}")
+            
         board[row][col] = current_player
         print_board(board)
+        
         if check_win(board, current_player):
-            print(current_player, "wins!")
+            print(f"{current_player} wins!")
             break
-        if " " not in [row[i] for row in board for i in range(3)]:
+        elif " " not in [cell for row in board for cell in row]:
             print("It's a tie!")
             break
+        
         current_player = players[(players.index(current_player) + 1) % 2]
 
 tic_tac_toe()
